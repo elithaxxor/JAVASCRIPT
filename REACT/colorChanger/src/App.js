fea0e0aee8b0497f3f1b07a2982560a94b61d15a
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-01-22 21:51:48 -0800

    Update App.js

--- REACT/colorChanger/src/App.js
+++ REACT/colorChanger/src/App.js
@@ -5,14 +5,25 @@ import './App.css';
 
 function App() {
 const [colorValue, setColor] = useState('');
+const [hexValue, setHexValue] = useState('');
+const [isDarkText, setDarkText] = useState(true);
+
 return (
   <div className="App">
     <Square 
     colorValue={colorValue} 
+    hexValue={hexValue}
+    isDarkText={isDarkText}
     />  
   <Input  
     colorValue={colorValue}
-    setColor={setColor} /> 
+    setColor={setColor} 
+    hexValue={hexValue}
+    setHexValue={setHexValue}
+    isDarkText={isDarkText}
+    setDarkText={setDarkText}
+
+    /> 
   </div>
 );
 }
