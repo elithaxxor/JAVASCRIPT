ff0487afe6288392b824a4745b492ac14df69fbe
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-01-28 14:20:16 -0800

    Update Header.js

--- REACT/LOGIN_VERIFY/src/components/Header.js
+++ REACT/LOGIN_VERIFY/src/components/Header.js
@@ -1,10 +1,19 @@
+import { Header as header0 } from "react-bootstrap";
+
 const Header = ({}) => {
   var time = new Date();
   return (
-    <p>
-      {" Current Time: "}
-      {time.getHours()}:{time.getMinutes()}::{time.getSeconds()}{" "}
-    </p>
+    <div class="card">
+      <div class="col-sm-0">
+        Quote
+        <blockquote class="blockquote mb-0">
+          <p style={{ color: "red" }}>Let Me Hack y0u!</p>
+          <header0 class="blockquote-footer">
+            Someone famous  - <cite title="Source Title">SourceCode</cite>
+          </header0>
+        </blockquote>
+      </div>
+    </div>
   );
 };
 
