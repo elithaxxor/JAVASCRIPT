72cb53ba4d9d233434f85a7ba4f06f923c801834
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-02-05 03:02:43 -0800

    Add files via upload

---
+++ REACT/BLOG_INCOMPLET/src/api/posts.js
@@ -0,0 +1,11 @@
+// take the post, and drill down the object (id) abnd the full post to <Post =
+// pass on the post and post(id]
+
+const posts = ( { post } ) => {
+    return(
+        <>
+teticals
+        </>
+    )
+}
+export default posts
