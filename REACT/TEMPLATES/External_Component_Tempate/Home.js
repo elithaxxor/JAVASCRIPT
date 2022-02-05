71a7d53c5fc986ee386b9b692d5f1bcccb69ec79
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-02-05 00:31:17 -0800

    Create Home.js

---
+++ REACT/TEMPLATES/External_Component_Tempate/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class home extends React.Component{
+    render(){
+        return(
+            <div>
+                random
+            </div>
+        );
+    }
+}
+
+export default Home
