9738f3f10e71fad1a26bf7cbd780d42a78f97c83
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-02-05 02:56:05 -0800

    Add files via upload

---
+++ REACT/REDUX_LOGIN/app/store.js
@@ -0,0 +1,12 @@
+import {configureStore} from "@reduxjs/toolkit"
+import userReducer from './slices/userSlice'
+
+export default configureStore({
+    reducer:{
+        user: {
+            userReducer,
+        },
+    }
+
+
+})
